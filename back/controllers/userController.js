38ef63f80fb021cc637041dcbdf1eb575bee62c3
Korablin Ivan <wan6sta@gmail.com>
2022-06-27 20:39:04 +0300

    some v

--- back/controllers/userController.js
+++ back/controllers/userController.js
@@ -1,7 +1,11 @@
+// @desc    Get user profile
+// @route   GET /api/users/profile
+// @access  Private
 export const getUserProfile = (req, res) => {
   const user = {
     name: 'Vanek',
-    age: 19
+    age: 19,
+    job: 'react developer'
   }
 
   res.json(user)
--- back/server.js
+++ back/server.js
@@ -3,11 +3,25 @@ import morgan from 'morgan'
 import dotenv from 'dotenv'
 import colors from 'colors'
 
+/* Routes */
+import userRoutes from "./routes/userRoutes.js";
+
 dotenv.config()
 
 const app = express()
 
-if (process.env.NODE_ENV === development)
+if (process.env.NODE_ENV === 'development')
   app.use(morgan('dev'))
 
 app.use(express.json())
+
+app.use('/api/users', userRoutes)
+
+const PORT = process.env.PORT || 5000
+
+app.listen(
+  PORT,
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+  )
+  )
